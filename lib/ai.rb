b343eca47d004fd54398ca521b8803c285a534f1
Patrick Hagmann <phagmann1@gmail.com>
2016-12-14 23:57:25 +0000

    small changes

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,4 +1,10 @@
 class Ai
+    def self.plan
+        num = 1 + rand(100)
+        return "Fold" if num <= 10
+        return "Call" if num > 10 && num <= 90
+        return "Raise" if num > 90
+    end
     
 
 end
